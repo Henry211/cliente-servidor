ca13380509f8a00d40f87cefb49ae32f0ed14f57
Henry <Henry211@users.noreply.github.com>
2021-04-16 18:47:25 -0600

    Mejorando el servidor

--- src/server-tcp.ts
+++ src/server-tcp.ts
@@ -3,7 +3,18 @@
 const webSocketServer = require('ws').Server;
 const wss = new webSocketServer({ port: 8081 });
 
+const respondToClient = () => {
+    const commonAnswers = [ 
+        
+    ];
+    //TODO: Completar esta función para que devuelva un string aleatorio del arreglo anterior
+};
+
+
 wss.on('connection', (ws: any) => {
+
+    console.log(ws._socket.remoteAddress);  // con esta info se identifica el cliente
+    console.log(ws._socket.remotePort);     //--- || ---
 
     ws.send('Hello Client');
   
